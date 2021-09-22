a609adbe5862513f0e05dbab78d25277506a6fc8
ujjawal09 <u2010g@gmail.com>
2021-09-22 19:23:01 +0530

    second commit

---
+++ LinkedList/src/basics/LinkedList.java
@@ -0,0 +1,69 @@
+package basics;
+
+public class LinkedList {
+
+	static Node head = null;
+
+	public static void addAtBeg(int data) {
+		Node temp = new Node(data);
+		if (head == null)
+			head = temp;
+		else {
+			temp.next = head;
+			head = temp;
+		}
+
+	}
+
+	public static void printLinkedList() {
+		Node temp = head;
+		if(temp==null)
+			System.out.println("Linked List doesn't exsist");
+		while (temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+
+	public static void addAtPos(int pos, int data) {
+		Node p = head;
+		Node temp = new Node(data);
+		pos--;
+		while (--pos > 0) {
+			p = p.next;
+		}
+		temp.next = p.next;
+		p.next = temp;
+
+	}
+
+	public static void deleteAtPos(int pos) {
+		
+		
+		Node p = head;
+		pos--;
+		while (--pos > 0) {
+			p = p.next;
+		}
+		p.next = p.next.next;
+
+	}
+
+	public static  void destroyLinkedList() {
+		head = null;
+	}
+
+	public static void main(String[] args) {
+		addAtBeg(5);
+		addAtBeg(10);
+		addAtBeg(15);
+		addAtPos(2, 11);
+		deleteAtPos(2);
+		printLinkedList();
+		destroyLinkedList();
+		printLinkedList();
+		
+		
+	}
+
+}
